42116ceb68d092fbe93327489c0939a72bae9686
Gusta <gustavorodrigues182@live.com>
2023-08-03 02:59:21 -0300

    Validando no Serializer

--- clientes/serializers.py
+++ clientes/serializers.py
@@ -6,3 +6,7 @@ class ClienteSerializer(serializers.ModelSerializer):
         model = Cliente
         fields = '__all__'
     
+    def validate_cpf(self, cpf): #instanciei e trouxe o CPF.
+        if len(cpf) != 11: #Se o tamanho(len) do CPF for != (Não for igual)
+            raise serializers.ValidationError('O CPF deve ter 11 dígitos') #Passando o erro que irá utilizar. 
+        return cpf
