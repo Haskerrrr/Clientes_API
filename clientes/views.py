f31a50388b1178f9dec8fe4f1a78132b1613e47a
Gusta <gustavorodrigues182@live.com>
2023-08-09 00:42:16 -0300

    Feat: Filter CPF

--- clientes/views.py
+++ clientes/views.py
@@ -7,5 +7,6 @@ class ClientesViewSet(viewsets.ModelViewSet):
     """Listando clientes"""
     queryset = Cliente.objects.all()
     serializer_class = ClienteSerializer
-    filter_backends = [DjangoFilterBackend, filters.OrderingFilter]
-    Ordering_fields = ['nome']
+    filter_backends = [DjangoFilterBackend, filters.OrderingFilter, filters.SearchFilter]
+    ordering_fields = ['nome']
+    search_fields = ['nome', 'cpf']
